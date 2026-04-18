98dd038742b9747834cd96600be4bd1d57370e99
cesarogiuseppeanto <cesaro_giuseppe_anto@libero.it>
2026-04-18 10:22:30 +0200

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -6,19 +6,24 @@ vectorizer = pickle.load(open("models/vectorizer.pkl", "rb"))
 dep_model = pickle.load(open("models/dep_model.pkl", "rb"))
 sent_model = pickle.load(open("models/sent_model.pkl", "rb"))
 
-# Test con una frase
-text = "La camera era pulita e il personale molto gentile"
+def predict_review(text):
+    # Preprocessing
+    text_clean = preprocess(text)
 
-# Preprocessing
-text_clean = preprocess(text)
+    # Vettorizzazione
+    text_vec = vectorizer.transform([text_clean])
 
-# Vettorizzazione
-text_vec = vectorizer.transform([text_clean])
+    # Predizioni
+    dep_pred = dep_model.predict(text_vec)[0]
+    sent_pred = sent_model.predict(text_vec)[0]
 
-# Predizioni
-dep_pred = dep_model.predict(text_vec)[0]
-sent_pred = sent_model.predict(text_vec)[0]
+    return dep_pred, sent_pred
 
-print("Testo:", text)
-print("Dipartimento:", dep_pred)
-print("Sentiment:", sent_pred)
+
+# test opzionale (NON necessario)
+if __name__ == "__main__":
+    text = "La camera era pulita e il personale molto gentile"
+    dep, sent = predict_review(text)
+    print("Testo:", text)
+    print("Dipartimento:", dep)
+    print("Sentiment:", sent)
