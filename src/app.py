24f2ef16d4837234df74909d18a696875f635054
cesarogiuseppeanto <cesaro_giuseppe_anto@libero.it>
2026-04-18 10:23:15 +0200

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
+from predict import predict_review
+
+st.title("Analisi recensioni hotel")
+
+text = st.text_area("Inserisci una recensione:")
+
+if st.button("Analizza"):
+    if text.strip() == "":
+        st.warning("Inserisci del testo")
+    else:
+        dep, sent = predict_review(text)
+        st.success(f"Reparto: {dep}")
+        st.success(f"Sentiment: {sent}")
